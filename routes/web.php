6827fdf4c205698ed0ac17a8d81b561afaceeb34
iqbalmaulanasidik990-de <iqbalmaulanasidik990@gmail.com>
2026-05-28 13:38:14 +0700

    basic route test

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QueueController;
 
 Route::get('/', function () {
     return 'QOFFEE ONLINE 🔥';
 });
-
-Route::get('/queue', function () {
-    return view('queue');
-});
-
-Route::post('/queue/store', [QueueController::class, 'store']);
-
-Route::get('/success/{id}', [QueueController::class, 'success']);
-
-Route::get('/admin', [QueueController::class, 'dashboard']);
-
-Route::get('/admin/status/{id}/{status}', [QueueController::class, 'updateStatus']);
-
-Route::get('/admin/delete/{id}', [QueueController::class, 'delete']);
-
-Route::get('/receipt/{id}', [QueueController::class, 'receipt']);
