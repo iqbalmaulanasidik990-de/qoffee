5c0ffb2be912aa6621031235345f2451805490eb
iqbalmaulanasidik990-de <iqbalmaulanasidik990@gmail.com>
2026-05-29 11:25:53 +0700

    test railway route

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,8 @@
+```php id="p6m1ws"
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\QueueController;
 
 Route::get('/', function () {
-    return view('home');
+    return 'QOFFEE ONLINE 🔥';
 });
-
-Route::get('/queue', function () {
-    return view('queue');
-});
-
-Route::post('/queue/store', [QueueController::class, 'store']);
-
-Route::get('/success/{id}', [QueueController::class, 'success']);
-
-Route::get('/admin', [QueueController::class, 'dashboard']);
-
-Route::get('/admin/status/{id}/{status}', [QueueController::class, 'updateStatus']);
-
-Route::get('/admin/delete/{id}', [QueueController::class, 'delete']);
-
-Route::get('/receipt/{id}', [QueueController::class, 'receipt']);
